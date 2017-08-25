46b2bad0f34bfd846cc61eec21b1cabfb087e2d5
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 08:26:56 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.get('/create-user', function (req, res) {
 
 app.get('/login', function (req, res) {
     res.send('Good');
+    res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
     
 });
 
