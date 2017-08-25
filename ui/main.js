f60646384c40b547543ff599c2264755e951dc86
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-24 20:39:11 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,28 @@
 console.log('Loaded!');
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.Done) {
+            
+            if (request.status === 200) {
+                alert('Logged in successfully')
+            } else if (request.readyState === 403) {
+                alert('Username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the sever');
+            }
+        }
+    };
+
+
+var username = document.getElementById('username').value;
+var password = document.getElementById('password').value;
+console.log(username);
+request.open('POST', 'http://datasci1204.imad.hasura-app.io/login',true);
+request.serRequestHeader('Content-Type', 'application/json');
+request.send(JSON.stringify({username: username, password: password}));
+
+};
