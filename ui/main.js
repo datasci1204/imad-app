1b83179ff3fa6a0f97e6bce96fed0437cd751895
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 17:34:37 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,6 +33,46 @@ request.send(null);
 */
 var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
+request.open('POST', 'http://datasci1204.imad.hasura-app.io/login',true);
+  request.setRequestHeader('Content-Type', 'application/json');
+  request.send(JSON.stringify({username: username, password: password}));
+
+};
+
+var create = document.getElementById('create_btn');
+
+create.onclick = function () {
+    var request = new XMLHttpRequest();
+    
+    
+    console.log(username);
+    
+    /*request.setRequestHeader('Content-Type', 'application/json');
+    --request.send(JSON.stringify({username: username, password: password}));
+*/
+    
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            
+            if (request.status === 200) {
+                var counter = request.responseText;
+                alert('Logged in successfully'+counter.toString());
+            } else if (request.readyState === 403) {
+                alert('Username/password is incorrect');
+            } else if (request.status === 500) {
+                alert('Something went wrong on the sever');
+            } else {
+                alert('User Created Successfully');
+            
+            }
+        }
+    };
+/*
+request.open('GET', 'http://datasci1204.imad.hasura-app.io/login',true);
+request.send(null); 
+*/
+var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
 request.open('POST', 'http://datasci1204.imad.hasura-app.io/create',true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify({username: username, password: password}));
