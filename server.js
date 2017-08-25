5404711e82d4d76304a9d3573e91ff48c500e781
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 11:40:38 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ app.get('/create-user', function (req, res) {
     
 
 app.get('/login', function (req, res) {
+    res.send('User successfully created:' + username);
    alert('Test is fine');
    console.log('Testing');
     
