ed67c3404fe70b21ff019570b0f94ab2e57e9471
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-24 21:24:22 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,13 +1,42 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser = require('body-parser');
+
+var config = {
+   user: 'datasci1204',
+   database: 'datasci1204',
+   host: 'db.imad.hasura-app.io',
+   post: '5432',
+   password: 'db-datasci1204-99885'
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app/get('/create-user', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, password], function (req, result){
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send('User successfully created:' + username);
+        }
+});
+});
+    
+
+
+
 
 app.get('/Article1.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'Article1.html'));
