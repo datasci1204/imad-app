d0a92e55f03c203d0c2ebb7e279d063717f9d4ef
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 16:20:03 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('/testdb', function(req, res){
       if(err){
           res.status(500).send(err.toString());
       } else {
-        res.send(JSON.stringify(result.rows));
+        res.send(JSON.stringify(result));
           
       }
       } 
