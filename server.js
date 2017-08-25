008689be21e48fcb91582549d8615f392006a547
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-24 21:57:14 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get('/login', function (req, res) {
     pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, password], function (req, result){
         if(err) {
             res.status(500).send(err.toString());
+            res.send('Not able to connect');
         } else {
             res.send('User successfully created:' + username);
         }
