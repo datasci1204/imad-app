a385fbb3974e80543cf732a79652fee876dd0d13
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 15:59:23 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ var crypto = require('crypto');
 var bodyParser = require('body-parser');
 
 var config = {
-   user: 'datasci1204',
-   database: 'datasci1204',
+   user: 'coco98',
+   database: 'coco98',
    host: 'db.imad.hasura-app.io',
    port: '5432',
    password: process.env.DB_PASSWORD
