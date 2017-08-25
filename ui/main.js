16101e5e2be26a562999ba9c95dcbd7965157c64
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 14:00:35 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,7 +13,7 @@ submit.onclick = function () {
 */
     
     request.onreadystatechange = function() {
-        if (request.readyState === XMLHttpRequest.Done) {
+        if (request.readyState === XMLHttpRequest.DONE) {
             
             if (request.status === 200) {
                 var counter = request.responseText;
