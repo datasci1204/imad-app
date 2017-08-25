851ee2e9b98fa5bc055f7daafa8db49ca86222f5
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-25 15:42:08 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var config = {
    database: 'datasci1204',
    host: 'db.imad.hasura-app.io',
    port: '5432',
-   password: 'db-datasci1204-99885'
+   password: process.env.DB_PASSWORD
     
 };
 
