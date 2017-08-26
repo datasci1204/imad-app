442e6d894b0d388432196268c68b8be396443723
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-26 19:46:24 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,6 +33,8 @@ request.send(null);
 */
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
+  console.log(username);
+  console.log(password);
   request.open('POST', 'http://datasci1204.imad.hasura-app.io/testdb',true);
   request.setRequestHeader("Content-Type", "application/json");
   request.send(JSON.stringify({username: username, password: password})); 
