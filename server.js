c78eae6e404a7072cc0ca99f7b57461a2966adaf
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-26 11:49:41 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/testdb22', function(req, res){
     pool.query('select * from "user" ', function(req, result){
       if(err){
           res.status(500).send(err.toString());
-          
+          res.send('check');
       } else {
        /* res.send(JSON.stringify(r)); */
        console.log(result);
