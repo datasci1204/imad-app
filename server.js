eeb5b8b0bd252bb7cd7504df9b9ab9bba8d54e81
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-27 09:49:54 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.post('/testdb', function(req, res){
       if(err){
           res.status(500).send(err.toString());
       } else {
-          if (result.rows.length === 0) {
+          if (result.rows.length > 0) {
               res.send(400).send('No user');
           } else {
               var dbString = result.row[0].password;
