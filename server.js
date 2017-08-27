08a3e7bdb288e0ff0c5f83a182e114c98bc2c7e2
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-27 12:28:46 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/login', function (req, res) {
     
     if(req.session && req.session.auth && req.session.auth.userId) {
     counter = counter + 1;
-    res.send('You are logged in').res.send(counter.toString());
+    res.send('You are logged in').send(counter.toString());
     }else {
         res.send('logged in to Continue').res.send(counter.toString());
     }
