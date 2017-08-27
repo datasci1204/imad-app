207c285436827ea32880251ae41c86e916941d53
datasci1204 <30935207+datasci1204@users.noreply.github.com>
2017-08-26 20:14:38 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post('/testdb', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
      
-    pool.query('select * from "user" where username = $1',  function(err, result){
+    pool.query('select * from "user" where username = "ABCD" ',  function(err, result){
       if(err){
           res.status(500).send(err.toString());
       } else {
